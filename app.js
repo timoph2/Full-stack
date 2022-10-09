a766a8b36396682b5428df2b2e1207e9c8c31079
tim <phuatimothy231@gmail.com>
2022-10-09 08:09:20 +0800

    'first'

---
+++ app.js
@@ -0,0 +1,41 @@
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').parse()
+// }
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+let indexRouter = require('./routes/index.js')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+// mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect('mongodb+srv://m002-student:m002-student@sandbox.0qtedq0.mongodb.net/?retryWrites=true&w=majority')
+
+const {Schema,model} = mongoose;
+const xxSchema = new Schema({
+  user: {
+    type: String,
+    unique: true // `email` must be unique
+  },
+  pw: String
+})
+
+
+app.use('/', indexRouter)
+
+
+
+app.listen(process.env.PORT || 3005, () =>{
+    console.log('listening on 3005!')
+})
+
+
+
+
